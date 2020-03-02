622e9ba5739b97cbe7c914e5110236cba77278a9
christopher <christophermms.ctt@gmail.com>
2020-03-02 14:47:36 -0300

    Root das paginas

---
+++ app/src/views/Home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="page-home">
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
+
+<style lang="stylus">
+  @import '../../assets/styl/config.styl';
+  @import '../../assets/styl/helpers.styl';
+  @import 'Home.styl';
+</style>
