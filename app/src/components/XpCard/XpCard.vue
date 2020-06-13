e28c7568fad77637673d2e5ca4a38e4e9706c6d4
fullstack-overkill <christophermms.ctt@gmail.com>
2020-06-13 16:09:58 -0300

    Experience start

---
+++ app/src/components/XpCard/XpCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="xp-card">
+    <div class="card-img"><img src="../../assets/img/xp/cquantt.png" alt=""></div>
+    <div class="card-content">
+      <div class="content-title">jr Full Stack</div>
+      <div class="content-desc">
+          <div class="desc-place-name">Cquantt</div>
+          <div class="desc-period">maio de 2019 - out de 2019</div>
+          <div class="desc-city">Goiânia e Região</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'XpCard',
+  props: {
+    Img: String,
+    Title: String,
+    Desc: String,
+    CtaLink: String,
+  },
+  data() {
+    return {
+
+    }
+  },
+
+}
+</script>
+
+<style lang="stylus">
+  @import '../../assets/styl/config.styl';
+  @import '../../assets/styl/helpers.styl';
+  @import 'XpCard.styl';
+</style>
---
+++ app/src/views/Experiences/Experiences.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="page-home">
+    <div class="header-aling">
+      <Header/>
+    </div>
+    <div class="main-aling">
+      <main class="Conteudo">
+        <XpCard/>
+        <XpCard/>
+        <XpCard/>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from "@/components/Header/Header.vue";
+import XpCard from "../../components/XpCard/XpCard.vue";
+
+export default {
+  name: 'Experiences',
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    Header,
+    XpCard,
+  }
+}
+</script>
+
+<style lang="stylus">
+  @import '../../assets/styl/config.styl';
+  @import '../../assets/styl/helpers.styl';
+  @import 'Experiences.styl';
+</style>
